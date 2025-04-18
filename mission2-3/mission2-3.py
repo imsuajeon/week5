d4cacf8efa59dffa787540874d20eeefa5f216af
imsuajeon <117347256+imsuajeon@users.noreply.github.com>
2025-04-19 01:14:15 +0900

    Update mission2-3.py

--- mission2-3/mission2-3.py
+++ mission2-3/mission2-3.py
@@ -1,36 +1,18 @@
-from gpiozero import LED, Button
+from gpiozero import LEDBoard, Button
 from time import sleep
 from signal import pause
 
-# 핀 정의
-red = LED(20)
-yellow = LED(16)
-green = LED(7)
-blue = LED(8)
+# 4개의 LED를 한꺼번에 묶어 leds 객체로 생성
+leds = LEDBoard(20, 16, 7, 8)
+
 button = Button(25, pull_up=True)
 
-def sequence_leds():
-    # 빨간 LED on / off
-    red.on()
-    sleep(1)
-    red.off()
+def domino4():
+    for led in leds:
+        led.on()
+        sleep(1)
+        led.off()
 
-    # 노란 LED on / off
-    yellow.on()
-    sleep(1)
-    yellow.off()
+button.when_pressed = domino4
 
-    # 초록 LED on / off
-    green.on()
-    sleep(1)
-    green.off()
-
-    # 파란 LED on / off
-    blue.on()
-    sleep(1)
-    blue.off()
-
-# 버튼 눌림 시 sequence_leds 함수 호출
-button.when_pressed = sequence_leds
-
-pause() 
+pause()
