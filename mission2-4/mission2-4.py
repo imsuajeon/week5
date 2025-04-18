1303673c14aff2ba101dba6f08e4c1df29cf03ee
imsuajeon <117347256+imsuajeon@users.noreply.github.com>
2025-04-19 01:47:23 +0900

    Update mission2-4.py

--- mission2-4/mission2-4.py
+++ mission2-4/mission2-4.py
@@ -1,8 +1,7 @@
 from gpiozero import LEDBoard, Button
 from signal import pause
 
-# 3개의 LED를 한 번에 묶어 leds 객체로 생성
-leds = LEDBoard(20, 16, 7)  # 순서대로 red, yellow, green
+leds = LEDBoard(20, 16, 7)
 button = Button(25, pull_up=True)
 
 i = 0  
