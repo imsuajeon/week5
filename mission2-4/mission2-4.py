83b5ea6023d6c4a493de9e1ab7a6afd28ff74cfb
imsuajeon <117347256+imsuajeon@users.noreply.github.com>
2025-04-21 16:47:22 +0900

    Update mission2-4.py

--- mission2-4/mission2-4.py
+++ mission2-4/mission2-4.py
@@ -1,7 +1,7 @@
 from gpiozero import LEDBoard, Button
 from signal import pause
 
-leds = LEDBoard(20, 16, 7)
+leds = LEDBoard(20, 16, 7, 8)
 button = Button(25, pull_up=True)
 
 i = 0  
@@ -9,8 +9,8 @@ i = 0
 def counter():
     global i
     print(i)
-    leds.value = [(i >> bit) & 1 for bit in range(3)] 
-    i = (i + 1) % 8
+    leds.value = [(i >> bit) & 1 for bit in range(4)] 
+    i = (i + 1) % 16
 
 button.when_pressed = counter
 pause()
